d0a6caa8468ff8f5aea68963e78fce20ef1f8b27
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-03-23 15:04:20 +0900

    * SimpleJob - start, next - .start(Step) : 처음 실행 할 step 설정, 최초한번 설정, SimpleJobBuilder 가 생성되고 반환 - .next(Step) : 다음에 실행 할 Step 들을 순차적으로 연결하도록 설정 : 여러 번 설정이 가능하며 모든 next()의 Step이 종료가 되면 Job이 종료된다

---
+++ src/main/java/com/example/springbatch/SimpleJobConfiguration.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * created by        : jws
+ * creation date     : 2022-01-04
+ *
+ * Copyright (c) 2021 Samsung SDS.
+ * All rights reserved.
+ *******************************************************************************/
+
+package com.example.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Class description
+ *
+ * @author jws
+ * @since 2022. 1. 4
+ * @version 1.0
+*/
+
+@Configuration
+@RequiredArgsConstructor
+public class SimpleJobConfiguration {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job job() {
+		return jobBuilderFactory.get("batchjob")
+				.start(step1())
+				.next(step2())
+				.next(step3())
+				.incrementer(new RunIdIncrementer())
+				.validator(new JobParametersValidator() {
+					@Override
+					public void validate(JobParameters parameters) throws JobParametersInvalidException {
+						// TODO Auto-generated method stub
+					}
+				})
+				.preventRestart()
+				.listener(new JobExecutionListener() {
+
+					@Override
+					public void beforeJob(JobExecution jobExecution) {
+						// TODO Auto-generated method stub
+
+					}
+
+
+					@Override
+					public void afterJob(JobExecution jobExecution) {
+						// TODO Auto-generated method stub
+
+					}
+				})
+				.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("step1 was executed");
+						return RepeatStatus.FINISHED;
+					}
+				})
+				.build();
+	}
+
+	@Bean
+	public Step step2() {
+		return stepBuilderFactory.get("step2").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("step2 was executed");
+				return RepeatStatus.FINISHED;
+			}
+		})
+		.build();
+	}
+
+
+
+	@Bean
+	public Step step3() {
+		return stepBuilderFactory.get("step3").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("step3 was executed");
+				return RepeatStatus.FINISHED;
+			}
+		})
+		.build();
+	}
+
+}
