a41a185a058d9bb4ca65b7d1c8652c9c4c25e85b
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-03-24 16:24:14 +0900

    * Step - StepBuilderFactory : StepBuilder를 생성하는 팩토리 클래스로서get(String name) 메서드 제공 : StepBuilderFactory.get("stepName") "stepName" 으로 step을 생성
    - StepBuilder(아래 다섯개 하위빌더)
    : Step을 구성하는 설정하는 조건에 따라 다섯 개의 하위 빌더 클래스를 생성하고 실제 Step 생성을 위임한다.
    - TaskletStepBuilder
    : TaskletStep 을 생성하는 기본 빌더 클래스
    - SimpleStepBuilder
    : TaskletStep 을 생성하며 내부적으로 청크기반의 작업을 처리한느 ChunkOrientedTasklet 클래스를
    생성한다.
    - PartitionStepBuilder
    : PartitionStep 을 생성하며 멀티 스레드 방식으로 JOb을 실행한다.
    - JobStepBuilder
    : JobStep 을 생성하며 Step 안에서 Job을 실행한다.
    - FlowStepBuilder
    : FlowStep을 생성하여 Step 안에서 Flow를 실행한다.

--- src/main/java/com/example/springbatch/StepBuilderJobConfiguration.java
+++ src/main/java/com/example/springbatch/StepBuilderJobConfiguration.java
@@ -8,6 +8,8 @@
 
 package com.example.springbatch;
 
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
@@ -24,6 +26,12 @@ import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,25 +48,17 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class SimpleJobConfiguration {
+public class StepBuilderJobConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job job() {
+	public Job batchJob() {
 		return jobBuilderFactory.get("batchjob")
+				.incrementer(new RunIdIncrementer())
 				.start(step1())
 				.next(step2())
-				.next(step3())
-				// CustomJobParametersValidator()
-				//.validator(new CustomJobParametersValidator())
-				// DefaultJobParametersValidator
-				//.validator(new DefaultJobParametersValidator(new String[] {"name","date"},  new String[] {"count"}))
-				// CustomJobParametersIncrementer()
-				//.incrementer(new CustomJobParametersIncrementer())
-				.incrementer(new RunIdIncrementer())
-				.preventRestart()
 				.build();
 	}
 
@@ -77,28 +77,65 @@ public class SimpleJobConfiguration {
 
 	@Bean
 	public Step step2() {
-		return stepBuilderFactory.get("step2").tasklet(new Tasklet() {
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step2 was executed");
-				return RepeatStatus.FINISHED;
-			}
-		})
-		.build();
+		return stepBuilderFactory.get("step2")
+				.<String, String>chunk(3)
+				.reader(new ItemReader<String>() {
+					@Override
+					public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+						return null;
+					}
+				})
+				.processor(new ItemProcessor<String, String>() {
+					@Override
+					public String process(String item) throws Exception {
+						return null;
+					}
+				})
+				.writer(new ItemWriter<String>() {
+					@Override
+					public void write(List<? extends String> items) throws Exception {
+
+					}
+				})
+				.build();
 	}
-
-
 
 	@Bean
 	public Step step3() {
-		return stepBuilderFactory.get("step3").tasklet(new Tasklet() {
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("step3 was executed");
-				return RepeatStatus.FINISHED;
-			}
-		})
-		.build();
+		return stepBuilderFactory.get("step3")
+				.partitioner(step1())
+				.build();
 	}
+
+	@Bean
+	public Step step4() {
+		return stepBuilderFactory.get("step4")
+				.job(job())
+				.build();
+	}
+
+	@Bean
+	public Step step5() {
+		return stepBuilderFactory.get("step5")
+				.flow(flow())
+				.build();
+	}
+
+	@Bean
+	public Job job() {
+		return jobBuilderFactory.get("job")
+				.start(step1())
+				.next(step2())
+				.next(step3())
+				.build();
+	}
+
+	@Bean
+	public Flow flow() {
+		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+		flowBuilder.start(step2()).end();
+		return flowBuilder.build();
+	}
+
 
 }
