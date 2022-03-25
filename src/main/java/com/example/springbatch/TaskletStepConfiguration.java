d5e3298cc36d2e47983150c1a05675a6daff1f5f
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-03-25 16:28:06 +0900

    * TaskletStep : 스프링 배차에서 제공하는 Step의 구현체로서, Tasklet을 실행시키는 도메인 객체 : RepeatTemplate를 사용해서 Tasklet의 구문을 트랜잭션 경계 내에서 반복해서 실행함 : Task 기반과 Chunk 기반으로 나누어서 Tasklet을 실행함
    - Task
    : 단일 작업 기반으로 처리되는 것이 효율 적임
    : RepeatTemplate -> Tasklet + Logic 반복실행 설정
    - Chunk
    : 하나의 큰 덩어리르 n개씩 나눠서 실행한다는 의미로 대량 처리에 효율 적임
    : RepeatTemplate -> ChunkOrientedTasklet + ItemReader + ItemProcessor +
    Item Writer 반복실행 설정

--- src/main/java/com/example/springbatch/TaskletStepConfiguration.java
+++ src/main/java/com/example/springbatch/TaskletStepConfiguration.java
@@ -8,30 +8,20 @@
 
 package com.example.springbatch;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -48,7 +38,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class StepBuilderJobConfiguration {
+public class TaskletStepConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
@@ -57,14 +47,13 @@ public class StepBuilderJobConfiguration {
 	public Job batchJob() {
 		return jobBuilderFactory.get("batchjob")
 				.incrementer(new RunIdIncrementer())
-				.start(step1())
-				.next(step2())
+				.start(taskStep())
 				.build();
 	}
 
 	@Bean
-	public Step step1() {
-		return stepBuilderFactory.get("step1")
+	public Step taskStep() {
+		return stepBuilderFactory.get("taskStep")
 				.tasklet(new Tasklet() {
 					@Override
 					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -75,67 +64,24 @@ public class StepBuilderJobConfiguration {
 				.build();
 	}
 
+
 	@Bean
-	public Step step2() {
-		return stepBuilderFactory.get("step2")
+	public Step chunkStep() {
+		return stepBuilderFactory.get("chunkStep")
 				.<String, String>chunk(3)
-				.reader(new ItemReader<String>() {
-					@Override
-					public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-						return null;
-					}
-				})
+				.reader(new ListItemReader<>(Arrays.asList("item1", "item2", "item3", "item4", "item5")))
 				.processor(new ItemProcessor<String, String>() {
 					@Override
 					public String process(String item) throws Exception {
-						return null;
-					}
+						return item.toUpperCase();
+					};
 				})
 				.writer(new ItemWriter<String>() {
 					@Override
 					public void write(List<? extends String> items) throws Exception {
-
+						items.forEach(item -> System.out.println(item));
 					}
 				})
 				.build();
 	}
-
-	@Bean
-	public Step step3() {
-		return stepBuilderFactory.get("step3")
-				.partitioner(step1())
-				.build();
-	}
-
-	@Bean
-	public Step step4() {
-		return stepBuilderFactory.get("step4")
-				.job(job())
-				.build();
-	}
-
-	@Bean
-	public Step step5() {
-		return stepBuilderFactory.get("step5")
-				.flow(flow())
-				.build();
-	}
-
-	@Bean
-	public Job job() {
-		return jobBuilderFactory.get("job")
-				.start(step1())
-				.next(step2())
-				.next(step3())
-				.build();
-	}
-
-	@Bean
-	public Flow flow() {
-		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
-		flowBuilder.start(step2()).end();
-		return flowBuilder.build();
-	}
-
-
 }
