70a442d1b60cf505039992c84dc6b30bdc973ba9
ws7307.jang <ws7307.jang@partner.samsung.com>
2022-03-28 17:31:09 +0900

    * JobStep : Job에 속하는 Step 중 외부의 Job을 포함하고 있는 step : 외부의 Job이 실패하면 Step이 실패하므로 결국 최종 기본 Job도 실패한다. : 모든 메타데이터는 기본 Job과 외부 Job별로 각각 저장된다. : 커다란 시스템을 작은 모듈로 쪼개고 job의 흐름을 관리하고자 할때 사용 할 수 있다.

--- src/main/java/com/example/springbatch/JobStepConfiguration.java
+++ src/main/java/com/example/springbatch/JobStepConfiguration.java
@@ -11,13 +11,19 @@ package com.example.springbatch;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
@@ -40,17 +46,52 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class Limit_AllowConfiguration {
+public class JobStepConfiguration {
 
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Job batchJob() {
-		return jobBuilderFactory.get("batchjob")
+	public Job parentJob() {
+		return jobBuilderFactory.get("parentJob")
 				.incrementer(new RunIdIncrementer())
-				.start(step1())
+				.start(jobStep(null))
 				.next(step2())
+				.build();
+	}
+
+	@Bean
+	public Step jobStep(JobLauncher jobLauncher) {
+		return stepBuilderFactory.get("jobStep")
+				.job(childJob()) // JobStep 내에서 실행 될 Job 설정, JobStepBuilder 반환
+				.launcher(jobLauncher) // Job을 실행할 JobLauncher
+				.parametersExtractor(jobParametersExtractor()) // Step의 ExecutiuonContext를 Job이 실행되는데 필요한 JobParameters로 변환
+				.listener(new StepExecutionListener() {
+					@Override
+					public void beforeStep(StepExecution stepExecution) {
+						stepExecution.getExecutionContext().putString("name", "user1");
+					}
+
+
+					@Override
+					public ExitStatus afterStep(StepExecution stepExecution) {
+						return null;
+					}
+				})
+
+				.build();
+	}
+
+	private DefaultJobParametersExtractor jobParametersExtractor() {
+		DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+		extractor.setKeys(new String[] {"name"});
+		return extractor;
+	}
+
+	@Bean
+	public Job childJob() {
+		return jobBuilderFactory.get("childJob")
+				.start(step1())
 				.build();
 	}
 
@@ -64,22 +105,19 @@ public class Limit_AllowConfiguration {
 						return RepeatStatus.FINISHED;
 					}
 				})
-				.allowStartIfComplete(true) // step1이 성공해도 true 옵션때문에 무조건 실행 됨
 				.build();
 	}
 
 	@Bean
 	public Step step2() {
 		return stepBuilderFactory.get("step2")
-				.tasklet(new Tasklet() {
+					.tasklet(new Tasklet() {
 					@Override
 					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 						System.out.println("step2 was executed");
-						throw new RuntimeCryptoException("step2 was failed");
-//						return RepeatStatus.FINISHED;
+						return RepeatStatus.FINISHED;
 					}
 				})
-				.startLimit(3) // 3번까지 step이 실행이 됨, 4번째 부터는 오류 발생
 				.build();
 	}
 
